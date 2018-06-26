22ccc56ec6a49979215f3b25b1180f865177937b
Kelvin Goodman <kelvin12goodman@gmail.com>
2018-06-26 14:19:19 -0400

    formatting

--- featureNormalize.m
+++ featureNormalize.m
@@ -1,25 +1,13 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
+  X_norm = X;
+  mu = zeros(1, size(X, 2));
+  sigma = zeros(1, size(X, 2));
 
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
-
-for i = 1 : size(X, 2)
-  mu(i) = mean(X(:,i));
-  X_norm(:,i) = X(:,i) - mu(i);
-  
-  sigma(i) = std(X(:,i));
-  X_norm(:,i) = X_norm(:,i) / sigma(i);
-endfor
-
-%fprintf(' %f \n', X_norm);
-
-
-
-
-
-
-
-% ============================================================
-
+  for i = 1 : size(X, 2)
+    mu(i) = mean(X(:,i));
+    X_norm(:,i) = X(:,i) - mu(i);
+    
+    sigma(i) = std(X(:,i));
+    X_norm(:,i) = X_norm(:,i) / sigma(i);
+  endfor
 end
