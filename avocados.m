d3df27cb920d17e9ede7368faf290170d4df1f9c
Kelvin Goodman <kelvin12goodman@gmail.com>
2018-07-08 19:31:17 -0400

    split data into training and validation sets

--- avocados.m
+++ avocados.m
@@ -3,7 +3,10 @@ fprintf('Loading Data ...\n');
 pkg load io;
 data = xlsread('avocadoData.xlsx', 'Sheet1', 'B2:T38');
 
-%Load data
+%Shuffle training data order
+data = data(randperm(size(data,1)),:);
+
+%Calculate X and y
 X = data(:,[1:4,6:16]); %remove 5th feature
 y = data(:,size(data, 2));
 m = length(y);
@@ -17,15 +20,21 @@ X = featureNormalize(X);
 % Add intercept term to X
 X = [ones(m, 1) X];
 
-%Shuffle training data orderfields
-XShuffled = X(randperm(size(X,1)),:);
-
 %X into XTrain and XValidate
+splitPoint = round(m * 2/3);
+%disp("Splitpoint: ");
+%disp(splitPoint);
+XTrain = X(1:splitPoint, :);
+XVal = X((splitPoint + 1):m, :);
+yTrain = y(1:splitPoint, :);
+yVal = y((splitPoint + 1):m, :);
+%disp(XTrain);
+%disp(yTrain);
 
 % Choose some alpha value
 alpha = 0.01;
 num_iters = 100000;
 
 % Init Theta and Run Gradient Descent 
-theta = zeros(columns(X), 1);
-gradientDescent(X, y, theta, alpha, num_iters)
+theta = zeros(columns(XTrain), 1);
+gradientDescent(XTrain, yTrain, theta, alpha, num_iters)
